c3b8fce9012140a481d0413742ca747e61f16749
Yusuf CAKIR <cakir2007@gmail.com>
2022-10-26 12:01:29 +0300

    Türkçe dil dosyaları güncellendi
    Türkçe dil dosyaları güncellendi.

--- tr/passwords.php
+++ tr/passwords.php
@@ -12,10 +12,10 @@ return [
     |
     */
 
-    'reset' => 'Parolanız sıfırlandı!',
-    'sent' => 'Parola sıfırlama bağlantınız e-posta ile gönderildi!',
+    'reset' => 'Şifreniz sıfırlandı!',
+    'sent' => 'Şifre sıfırlama bağlantınız e-posta ile gönderildi!',
     'throttled' => 'Lütfen tekrar denemeden önce bekleyin.',
-    'token' => 'Parola sıfırlama adresi/kodu geçersiz.',
-    'user' => 'Bu e-posta adresi ile kayıtlı bir üye bulunmuyor.',
+    'token' => 'Şifre sıfırlama linki/kodu geçersiz.',
+    'user' => 'Bu e-posta adresi ile bir kullanıcı bulunmuyor.',
 
 ];
